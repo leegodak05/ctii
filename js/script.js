57af745fc505c7932a488177efbe043717152caa
leegodak05 <tjd51392@naver.com>
2024-11-07 16:28:01 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+ // 햄버거
+ $(function(){
+    $('.hamburger_button').click(function(){
+        $(this).toggleClass('active');
+        $('.overlay').toggleClass('visible');
+    });
+});
+
+
+var swiper = new Swiper(".correspondent", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
